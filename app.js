a4ff8565ef94d5949028d7408422495a6faa16e2
inzamam malik <malikasinger1@github.com>
2016-01-06 22:11:18 +0500

    working on home page

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-angular.module("todoApp",["ui.router"])
+angular.module("todoApp",["ui.router","ngMaterial"])
 
 .config(function($urlRouterProvider , $stateProvider){
 
     $urlRouterProvider.otherwise("/home");
 
-    $stateProvider.
-        state("home",{
+    $stateProvider
+        .state("home",{
         url : "/home",
         templateUrl : "views/home/home.html",
         controller : "homeController"
---
+++ views/home/home.js
@@ -0,0 +1,6 @@
+
+
+angular.module("todoApp")
+.controller("homeController",function($scope){
+
+});
