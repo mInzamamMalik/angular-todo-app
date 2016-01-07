18f2d62c6c04a9a377c603a3c04fbd6f4a342cd1
Inzamam Malik <malikasinger1@gmail.com>
2016-01-07 21:39:48 +0500

    working on home page

--- views/home/home.js
+++ views/home/home.js
@@ -3,23 +3,17 @@
 angular.module("todoApp")
 .controller("homeController",function($scope){
 
+    $scope.newEntry = "";
+
     $scope.todoList =
         [
             {checked : false ,text: "this is a hard coded todo"} ,
             {checked : false ,text: "this is another todo"}
         ];
 
-
-    $scope.listToBeDelete = [];
-
-    $scope.onCheck = function(){
-        alert("dg");
-    };
-
-
     $scope.add = function(){
             if($scope.newEntry){
-                $scope.todoList.push($scope.newEntry);
+                $scope.todoList.push(   {checked : false, text: $scope.newEntry}   );
                 $scope.newEntry = "";
             }
 
