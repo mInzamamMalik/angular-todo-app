8fc247382743911b4e8cc4f90e06f8c444ab4951
inzamam malik <malikasinger@gmail.com>
2016-01-08 02:45:28 +0500

    fdgfd

--- views/home/home.js
+++ views/home/home.js
@@ -16,6 +16,28 @@ angular.module("todoApp")
                 $scope.todoList.push(   {checked : false, text: $scope.newEntry}   );
                 $scope.newEntry = "";
             }
+        };
 
+    $scope.archive = function(){
+        for(var i=0; i<$scope.todoList.length ; i++){
+            if($scope.todoList[i].checked){
+                $scope.todoList.splice(i,1);
+                i--;
+            }
         }
+    };
+
+    $scope.$watch("todoList", function( newValue, oldValue ) {
+            $scope.remaining = 0;
+            for(i=0; i<newValue.length; i++){
+                if(newValue[i].checked){
+                    $scope.remaining++;
+                }
+            }
+        },
+        true // Object equality (not just reference).
+    );
+
+
+
 });
