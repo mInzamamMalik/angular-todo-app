b8416ca7edfb679ad06ff9503fd6fe355cf0d8fb
inzamam malik <malikasinger@gmail.com>
2016-01-07 12:12:48 +0500

    fdgfd

--- views/home/home.js
+++ views/home/home.js
@@ -3,4 +3,25 @@
 angular.module("todoApp")
 .controller("homeController",function($scope){
 
+    $scope.todoList =
+        [
+            {checked : false ,text: "this is a hard coded todo"} ,
+            {checked : false ,text: "this is another todo"}
+        ];
+
+
+    $scope.listToBeDelete = [];
+
+    $scope.onCheck = function(){
+        alert("dg");
+    };
+
+
+    $scope.add = function(){
+            if($scope.newEntry){
+                $scope.todoList.push($scope.newEntry);
+                $scope.newEntry = "";
+            }
+
+        }
 });
